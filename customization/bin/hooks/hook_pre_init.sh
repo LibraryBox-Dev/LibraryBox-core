6feaef2391eabb5ecfd6185c0655c26ecb05e6cc
Matthias Strubel <matthias.strubel@aod-rpg.de>
2014-09-08 07:25:53 +0200

    Generate link between index.html and redirect.html once during init 	Closed #19

--- customization/bin/hooks/hook_pre_init.sh
+++ customization/bin/hooks/hook_pre_init.sh
@@ -30,5 +30,10 @@ cp -rv  $PIRATEBOX_FOLDER/www_old/cgi-bin $WWW_FOLDER/cgi-bin
 # Link to the USB-Stick
 ln -s $WWW_CONTENT  $WWW_FOLDER/content
 
+# link index.html with redirect.html
+if [ ! -e  $WWW_CONTENT/index.html ] ; then
+	ln -s $WWW_CONTENT/redirect.html  $WWW_CONTENT/index.html
+fi
+
 $PIRATEBOX_FOLDER/bin/ftp_enable.sh generate
 
